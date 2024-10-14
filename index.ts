afa96e4d03f6eda9b569c2a0b18b2e024e355980
gurpreet991 <gurpreet.singh@ellocentlabs.com>
2024-10-14 19:30:44 +0530

    feat(changeStatusBarColor): function implemenation added

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-export {};
+const changeStatusBarColor = (color: string) => {
+  try {
+    if (!color) {
+      return;
+    }
+
+    window.ReactNativeWebView.postMessage(
+      JSON.stringify({ event: "barColor", color })
+    );
+  } catch (error) {
+    throw error;
+  }
+};
+
+export { changeStatusBarColor };
+
+declare global {
+  interface Window {
+    ReactNativeWebView: {
+      postMessage: (message: string) => void;
+    };
+  }
+}
